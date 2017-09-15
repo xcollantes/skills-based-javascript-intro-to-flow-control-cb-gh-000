3a9f762a004f4cc58b24ffdf12ce292ce30c3e33
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-15 23:26:55 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -17,7 +17,17 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  return age >= 13 && age <= 19 ? "You are a teenager!" : "You are a kid";
+  if (age >= 13 && age <= 19)
+  {
+    return "You are a teenager!";
+  }
+  else if (age <= 13)
+  {
+    return "You are a kid";
+  }
+  else {
+    return ("You are a grownup")
+  }
 }
 
 function ternaryTeenager(age) {
