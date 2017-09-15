ae2c037d8fde0ab6d86bafcdc7d5212ee796dfb7
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-15 23:29:07 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -35,5 +35,13 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case age >= 13 && age <= 19:
+      return "You are a teenager";
+      
+      break;
+    default:
+      return "You have an age"
+      
+  }
 }
